81f7a295ea7b7fda32e82bc4b73a342d3ed409fc
HongCFull <72356183+HongCFull@users.noreply.github.com>
2021-09-24 20:21:25 +0800

    modified pivot point of the connector to bottom mid

--- Assets/Scripts/ProceduralDungeonGeneration/CorridorConnector.cs
+++ Assets/Scripts/ProceduralDungeonGeneration/CorridorConnector.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// The GameObject attached with this script should in the bottom mid of the connector.
+/// </summary>
 public class CorridorConnector : MonoBehaviour
 {
     [SerializeField] private Vector2 corridorSize;
@@ -11,15 +14,20 @@ public class CorridorConnector : MonoBehaviour
         
         //Draw forward vector at the centroid of the corridor
         Gizmos.color = Color.green;
-        Gizmos.DrawLine(transform.position, transform.position + transform.forward);
+        Gizmos.DrawLine(transform.position + transform.up*corridorSize.y/2, transform.position + transform.up*corridorSize.y/2 + transform.forward);
         
         //Draw Boarders
-        Vector3 leftUp = transform.position - transform.right * corridorSize.x / 2 + transform.up * corridorSize.y / 2;
-        Vector3 leftDown = transform.position - transform.right * corridorSize.x / 2 - transform.up * corridorSize.y / 2;
+//        Vector3 leftUp = transform.position - transform.right * corridorSize.x / 2 + transform.up * corridorSize.y / 2;
+//        Vector3 leftDown = transform.position - transform.right * corridorSize.x / 2 - transform.up * corridorSize.y / 2;
 
-        Vector3 rightUp = transform.position + transform.right * corridorSize.x / 2 + transform.up * corridorSize.y / 2;
-        Vector3 rightDown = transform.position + transform.right * corridorSize.x / 2 - transform.up * corridorSize.y / 2;
-        
+//        Vector3 rightUp = transform.position + transform.right * corridorSize.x / 2 + transform.up * corridorSize.y / 2;
+//        Vector3 rightDown = transform.position + transform.right * corridorSize.x / 2 - transform.up * corridorSize.y / 2;
+        Vector3 leftUp = transform.position - transform.right * corridorSize.x / 2 + transform.up * corridorSize.y;
+        Vector3 leftDown = transform.position - transform.right * corridorSize.x / 2 ;
+
+        Vector3 rightUp = transform.position + transform.right * corridorSize.x / 2 + transform.up * corridorSize.y ;
+        Vector3 rightDown = transform.position + transform.right * corridorSize.x / 2 ;
+
         Gizmos.color = Color.cyan;
 
         Gizmos.DrawLine(leftUp, rightUp);
