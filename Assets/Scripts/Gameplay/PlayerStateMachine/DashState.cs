a3c488eddd2884ea832ad3ed6290b633d552d44f
HongCFull <72356183+HongCFull@users.noreply.github.com>
2021-11-01 18:40:14 +0800

    fixed idle walk run jump state rolling issue

--- Assets/Scripts/Gameplay/PlayerStateMachine/DashState.cs
+++ Assets/Scripts/Gameplay/PlayerStateMachine/DashState.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TPSTemplate;
 using UnityEngine;
 
 public class DashState : StateMachineBehaviour
@@ -7,27 +8,42 @@ public class DashState : StateMachineBehaviour
     private bool originalRMOption;
     private int animIDCachedDashAngle;
     private int animIDTurningAngle;
+    private ThirdPersonController tpsController;
 
      public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         base.OnStateEnter(animator, stateInfo, layerIndex);
-        animIDCachedDashAngle = Animator.StringToHash("CachedDashAngle");
-        animIDTurningAngle = Animator.StringToHash("TurningAngle");
-        
+
+        InitializeVariables(animator);
+ 
         animator.SetFloat(animIDCachedDashAngle,animator.GetFloat(animIDTurningAngle));
-        originalRMOption = animator.applyRootMotion;
         animator.applyRootMotion = true;
-Debug.Log("enter dash state with rm: "+animator.applyRootMotion);
+        tpsController.DisableCharacterWalking();
+
+        //Debug.Log("enter dash state with rm: "+animator.applyRootMotion);
         
     }
      
 
      public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
+     {
         base.OnStateExit(animator, stateInfo, layerIndex);
-        Debug.Log("exit dash state with rm"+animator.applyRootMotion);
+       // Debug.Log("exit dash state with rm"+animator.applyRootMotion);
+        
+        tpsController.EnableCharacterWalking();
         animator.applyRootMotion = originalRMOption;
         animator.SetFloat(animIDCachedDashAngle,0f); 
 
-    }
+     }
+
+     private void InitializeVariables(Animator animator)
+     {
+         tpsController = animator.GetComponent<ThirdPersonController>();
+         
+         animIDCachedDashAngle = Animator.StringToHash("CachedDashAngle");
+         animIDTurningAngle = Animator.StringToHash("TurningAngle");
+         
+         originalRMOption = animator.applyRootMotion;
+
+     }
 }
